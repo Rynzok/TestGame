40ff69807e073328e53789415bd59b4d160725e8
Виктор Киселёв <vkisel102@yandex.ru>
2023-07-27 12:54:06 +0300

    Осталось добавить потоки и построение гистограммы

--- TestGame.cpp
+++ TestGame.cpp
@@ -5,18 +5,56 @@
 #include <ctime>
 #include <random>
 #include <functional>
+#include <chrono>
 
 using namespace std;
+
+
+int prize[11]{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+mt19937::result_type seed = time(0);
+auto dice_rand = bind(uniform_int_distribution<__int64>(0, 10000000000),
+    mt19937(seed));
+
+void Play()
+{
+    __int64 a[12]{0, 8461775783, 8500000000, 8696509704, 9341857710, 9657585612, 9982328470, 9991766292, 9992773662, 9993788806,9994016324, 10000000000};
+    for (int i = 0; i < 10000; i++)
+    {
+
+        cout << dice_rand() << endl;
+        if (a[0] <= dice_rand() && dice_rand() < a[1]) prize[0]++;
+        if (a[1] <= dice_rand() && dice_rand() < a[2]) prize[1]++;
+        if (a[2] <= dice_rand() && dice_rand() < a[3]) prize[2]++;
+        if (a[3] <= dice_rand() && dice_rand() < a[4]) prize[3]++;
+        if (a[4] <= dice_rand() && dice_rand() < a[5]) prize[4]++;
+        if (a[5] <= dice_rand() && dice_rand() < a[6]) prize[5]++;
+        if (a[6] <= dice_rand() && dice_rand() < a[7]) prize[6]++;
+        if (a[7] <= dice_rand() && dice_rand() < a[8]) prize[7]++;
+        if (a[8] <= dice_rand() && dice_rand() < a[9]) prize[8]++;
+        if (a[9] <= dice_rand() && dice_rand() < a[10]) prize[9]++;
+        if (a[10] <= dice_rand() && dice_rand() <= a[11]) prize[10]++;
+    }
+
+
+}
 
 int main()
 {
 
-    mt19937::result_type seed = time(0);
-    auto dice_rand = bind(uniform_int_distribution<int>(1, 10),
-        mt19937(seed)); // Оболочка для класса mt19937, что бы генерировать чилса в необходимом диапазоне
-    cout << dice_rand();
+    //cout << dice_rand() << endl ;
+    //winning w;
+    //w.Print();
+    Play();
+    for (int i = 0; i < 11; i++)
+    {
+        cout << prize[i] << ' ';
+    }
+
 
 }
+
+
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
